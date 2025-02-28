b0ee3c2e6a8a86628c7a8970329f055cb9eaac55
TanglyEagle7718 <rishi.borra@outlook.com>
2025-02-28 02:46:31 -0500

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+git remote rm origin
+git remote add origin https://github.com/TanglyEagle7718/WatchParty.git
+
+npm run deploy 
+
+git remote rm origin
+git remote add origin git@github.com:TanglyEagle7718/WatchParty.git
+
